c294a8d8d3169faed182ca48dde2ab31d2aa5de7
JOÃO VICTOR ZIGNAGO <2020315234@IFAL.LOCAL>
2022-08-29 11:53:31 -0300

    Iniciando repositório

---
+++ src/components/infoCard.js
@@ -0,0 +1,16 @@
+import { Text } from "react-native"
+import { View } from "react-native-web"
+
+
+export default function infoCard() {
+    return (
+        <View style={styles.mainContainer}>
+            <View style ={styles.mainInfo}></View>
+            <View style ={styles.subCardsContainer}></View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
